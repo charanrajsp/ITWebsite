0d7ee87a04ced6f692985e8a07278ee7515e94de
CHARANRAJ PATTAR <68002663+charanrajsp@users.noreply.github.com>
2025-02-07 19:41:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import cors from "cors";
 import chatRoutes from "./routes/chatRoutes.js"; // Import chatbot routes
 
 const app = express();
-app.use(cors());
+const coreOption =['https://it-website-three.vercel.app/']
+app.use(cors(coreOption));
 app.use(express.json());
 
 app.use("/chat", chatRoutes); // Set up chatbot routes
